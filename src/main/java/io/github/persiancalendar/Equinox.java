0aeda806d5cd3768d98e116a18df4a83c3a51dde
Ebrahim Byagowi <ebrahim@gnu.org>
2021-12-30 15:36:17 +0330

    Make equinox millisecond zero
    Makes tests more reliable.

--- src/main/java/io/github/persiancalendar/Equinox.java
+++ src/main/java/io/github/persiancalendar/Equinox.java
@@ -85,6 +85,7 @@ public class Equinox {
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
         calendar.set(Calendar.SECOND, second);
+        calendar.set(Calendar.MILLISECOND, 0);
         return calendar.getTime();
     }
 
