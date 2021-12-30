0db00c885d6a5b9a106c3624674f5ebdf0ecce3c
Ebrahim Byagowi <ebrahim@gnu.org>
2021-12-30 15:22:53 +0330

    Extend and update the tests

--- src/test/java/io/github/persiancalendar/MainTests.java
+++ src/test/java/io/github/persiancalendar/MainTests.java
@@ -11,26 +11,48 @@ public class MainTests {
     public final void test_equinox_time() {
         Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("Asia/Tehran"));
         int[][] elements = new int[][]{
-                // https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201399-Full.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1401.pdf
+                {2022, 3, 20, 19, 3/*should be 19*/, 3/* should be 26*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1400.pdf
+                {2021, 3, 20, 13, 7/*should be 19*/, 14/* should be 28*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1399.pdf
                 {2020, 3, 20, 7, 20/*should be 19*/, 43/* should be 37*/},
-                // https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201398-Full.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1398.pdf
                 {2019, 3, 21, 1, 28, 13/*should be 27*/},
-                // https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201397-Full.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1397.pdf
                 {2018, 3, 20, 19, 45, 53/*should be 28*/},
-                // https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201396-Full.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1396.pdf
                 {2017, 3, 20, 13, 59/*should be 58*/, 38/*should be 40*/},
-                // https://calendar.ut.ac.ir/Fa/News/Data/Doc/Calendar%201395-Full.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1395.pdf
                 {2016, 3, 20, 8, 0, 55/*should be 12*/},
-                // http://vetmed.uk.ac.ir/documents/203998/204600/calendar-1394.pdf
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1394.pdf
                 {2015, 3, 21, 2, 16/*should be 15*/, 0/*should be 11*/},
-                // https://raw.githubusercontent.com/ilius/starcal/master/plugins/iran-jalali-data.txt
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1393.pdf
                 {2014, 3, 20, 20, 27, 41/*should be 7*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1392.pdf
                 {2013, 3, 20, 14, 32/*should be 31*/, 41/*should be 56*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1391.pdf
                 {2012, 3, 20, 8, 44, 19/*should be 27*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1390.pdf
                 {2011, 3, 21, 2, 51/*should be 50*/, 38/*should be 25*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1389.pdf
                 {2010, 3, 20, 21, 2, 49/*should be 13*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1388.pdf
                 {2009, 3, 20, 15, 14/*should be 13*/, 50/*should be 39*/},
-                {2008, 3, 20, 9, 18, 17/*should be 19*/}
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1387.pdf
+                {2008, 3, 20, 9, 18, 17/*should be 19*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1386.pdf
+                {2007, 3, 21, 3, 37, 16/*should be 26*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1385.pdf
+                {2006, 3, 20, 21, 55, 19/*should be 35*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1384.pdf
+                {2005, 3, 20, 16, 4/*should be 3*/, 37/*should be 24*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1383.pdf
+                {2004, 3, 20, 10, 19/*should be 18*/, 41/*should be 37*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1382.pdf
+                {2003, 3, 21, 4, 30/*should be 29*/, 22/*should be 45*/},
+                // https://calendar.ut.ac.ir/Fa/Tyear/Data/full-1381.pdf
+                {2002, 3, 20, 22, 46, 12/*should be 2*/}
         };
 
         for (int[] item : elements) {
